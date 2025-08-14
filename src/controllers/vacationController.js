efb5621941d6bc2fcfcf53c92445a007641b8acf
Julio <jteran@cuevatech.com>
2025-08-14 16:08:26 -0500

    "edpoint vacaciones terminados"

--- src/controllers/vacationController.js
+++ src/controllers/vacationController.js
@@ -1,4 +1,5 @@
 const prisma = require("../config/db");
+const { get } = require("../routes/userRoutes");
 const {sendEmail}  = require("../utils/mailer");
 
 async function createVacation(req, res) { 
@@ -72,6 +73,8 @@ async function sendVacationRequestEmail(vacation) {
      //   where: { isSuperuser: true },
       //  select: { email: true }
     //});
+
+    //Este correo debería ir al de recursos humanos o al administrador (mi correo personal por ahora) 
     const emailAdmin = "juliocesarteranlinares55@gmail.com"
     const user = await prisma.user.findUnique({
         where: { id: vacation.userId },
@@ -85,8 +88,115 @@ async function sendVacationRequestEmail(vacation) {
     <p>Por favor, <br> revisa y cambia el estado de la solicitud</br>.</p>
     `;
     
-    await sendEmail(emailAdmin, 'Solicitud de Vacaciones!!!', message);
+    await sendEmail(emailAdmin, 'Atender solicitud de vacaciones', message);
 }       
 
 
-module.exports = { createVacation };
+async function sendVacationStatusEmail(vacation) {
+    const user = await prisma.user.findUnique({
+        where: { id: vacation.userId },
+        select: { email: true, firstName: true, lastName: true }
+    });
+
+    const message = `<h1>Estado de Solicitud de Vacaciones</h1>
+    <p>Hola ${user.firstName} ${user.lastName},</p>
+    <p>Tu solicitud de vacaciones del ${vacation.startDate.toISOString().split('T')[0]} al ${vacation.endDate.toISOString().split('T')[0]}</p>
+    <p>Ha sido ${vacation.status === 'APPROVED' ? 'aprobada' : 'rechazada'} por el administrador.</p>
+    <p>Gracias.</p>
+    `;
+
+    sendEmail(user.email, 'Estado de Solicitud de Vacaciones', message);
+}
+
+
+async function updateVacation(req, res) {
+    const { id } = req.params;
+    const { status } = req.body;
+    const vacation = await prisma.vacation.findUnique({
+        where: { id: parseInt(id) },
+        select: { userId: true , startDate : true , endDate: true }
+    });
+
+    if (!vacation) {
+        return res.status(404).json({ error: 'Solicitud de vacaciones no encontrada.' });
+    }
+
+    if (status !== 'APPROVED' && status !== 'DENIED') {
+        return res.status(400).json({ error: 'Estado inválido. Debe ser APPROVED o DENIED.' });
+    }
+
+    try {
+        const updatedVacation = await prisma.vacation.update({
+            where: { id: parseInt(id) },
+            data: { status },
+        });
+
+        const diffTime = updatedVacation.endDate.getTime() - updatedVacation.startDate.getTime();
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1; // +1 para incluir el primer día
+
+
+
+        // Si la solicitud es aprobada, actualizar los días disponibles del usuario
+        if (status === 'APPROVED') {
+            await prisma.user.update({
+                where: { id: vacation.userId },
+                data: { vacationDayAvailable: { decrement: diffDays } }
+            });
+        }
+        
+        
+        console.log("Correo enviado al usuario sobre el estado de su solicitud de vacaciones");
+
+        res.status(200).json({ message: "Solicitud de vacaciones actualizada con éxito", updatedVacation });
+         // Enviar correo al usuario sobre el estado de su solicitud
+         sendVacationStatusEmail(updatedVacation);
+
+    } catch (error) {
+        console.error("Error al actualizar solicitud de vacaciones:", error);
+        return res.status(500).json({ message: "Error interno del servidor" });
+    }
+
+}
+
+async function getVacations(req, res) {
+    try {   
+        const vacations = await prisma.vacation.findMany({
+            include: {
+                user: {
+                    select: {
+                        firstName: true,
+                        lastName: true,
+                        email: true
+                    }
+                }
+            }
+        });
+        return res.status(200).json(vacations);
+    } catch (error) {
+        console.error("Error al obtener las solicitudes de vacaciones:", error);
+        return res.status(500).json({ message: "Error interno del servidor" });
+    }}
+
+    async function getMyVacations(req, res) {
+        try {
+            const userId = req.user.userId;
+            const vacations = await prisma.vacation.findMany({
+                where: { userId },
+                include: {
+                    user: {
+                        select: {
+                            firstName: true,
+                            lastName: true,
+                            email: true
+                        }
+                    }
+                }
+            });
+            return res.status(200).json(vacations);
+        } catch (error) {
+            console.error("Error al obtener mis solicitudes de vacaciones:", error);
+            return res.status(500).json({ message: "Error interno del servidor" });
+        }
+    }
+
+module.exports = { createVacation , updateVacation , getVacations , getMyVacations }; 
--- src/routes/vacationRoutes.js
+++ src/routes/vacationRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middlewares/authenticateToken");
+const authorizeSuperUser = require("../middlewares/authorizeSuperUser");
 const validate = require("../middlewares/validate");
 const { vacationSchema } = require("../schemas/vacationSchema");
-const { createVacation } = require("../controllers/vacationController");
+const { createVacation , updateVacation , getVacations , getMyVacations} = require("../controllers/vacationController");
 
+router.get("/", authenticateToken, authorizeSuperUser, getVacations);
+router.get("/me", authenticateToken, authorizeSuperUser, getMyVacations);
 router.post("/", authenticateToken, validate(vacationSchema), createVacation);
+router.put("/:id", authenticateToken, authorizeSuperUser , updateVacation);
 
 
 module.exports = router;
